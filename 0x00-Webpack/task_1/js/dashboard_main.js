04fad0b36c6c85e7af5e127f2254c8e7ed402945
Michael Hudson <mnkotagu@gmail.com>
2023-10-23 17:36:00 +0300

    Improve files for Task 1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,17 @@
 const $ = require( "jquery" );
 const _ = require('lodash');
 
-let count = 0;
-
-function updateCounter() {
-    count++;
-    $( "#count" ).text(`${count} clicks on the button` );
-}
-
-
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id=\'count\'></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
+
+
+let count = 0;
+function updateCounter() {
+    count++;
+    $( "#count" ).text(`${count} clicks on the button` );
+}
+
 $('button').click(_.debounce(updateCounter, 500));
