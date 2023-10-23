67d66ed28ed6cb077a1c504989380f7c65d600e5
Michael Hudson <mnkotagu@gmail.com>
2023-10-23 22:49:54 +0300

    Implement solution for  Task 4 - Dev servers, modules, and tree shaking

---
+++ 0x00-Webpack/task_3/modules/body/body.js
@@ -0,0 +1,14 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import './body.css';
+
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+let counter = 0;
+
+function updateCounter() {
+    counter++;
+    $("#count").html(`${counter} clicks on the button`);
+}
+
+$('button').on('click', _.debounce(updateCounter, 500));
