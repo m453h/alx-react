d132eb7a7146f905dc2ff69c32ce2d663fd36ad0
Michael Hudson <mnkotagu@gmail.com>
2023-10-23 19:35:12 +0300

    Implement improved solution for task 1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,21 +1,18 @@
 const $ = require("jquery");
 const _ = require('lodash');
 
-var count = 0;
-
-function updateCounter() {
-    count++;
-    $("#count").text(`${count} clicks on the button`);
-}
-
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-
-const $button = $("<button type='button'>Click here to get started</button>").on(
-    'click',
-    _.debounce(updateCounter, 500, {'leading': true, 'trailing': false})
-);
-
-$('body').append($button);
-$('body').append("<p id='count'></p>");
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count' data-click-count='0'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
+
+
+function updateCounter() {
+    let count = parseInt($('#count').data('click-count'));
+    count++;
+    $("#count").html(`${count} clicks on the button`);
+    $('#count').data('click-count', count);
+}
+
+$('button').on('click', _.debounce(updateCounter, 500));
