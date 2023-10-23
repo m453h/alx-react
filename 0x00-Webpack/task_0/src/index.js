3494320c3716858d937e12504a808ea6e702dd49
Michael Hudson <mnkotagu@gmail.com>
2023-10-23 16:19:26 +0300

    Improve index.js and index.html for basic setup task

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,8 +1,6 @@
 const $ = require( "jquery" );
 
-$(function() {
-    const mySelector = $('body');
-    mySelector.append('<p>Holberton Dashboard</p>');
-    mySelector.append('<p>Dashboard data for the students</p>');
-    mySelector.append('<p>Copyright - Holberton School</p>');
-});
+const mySelector = $('body');
+mySelector.append('<p>Holberton Dashboard</p>');
+mySelector.append('<p>Dashboard data for the students</p>');
+mySelector.append('<p>Copyright - Holberton School</p>');
