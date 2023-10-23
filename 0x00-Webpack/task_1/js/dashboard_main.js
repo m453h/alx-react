325a5447c5ab0eaebf4842f0818cbac07e13d1de
Michael Hudson <mnkotagu@gmail.com>
2023-10-23 17:53:33 +0300

    Attempt to set click event for single button created

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,19 @@
-const $ = require( "jquery" );
+const $ = require("jquery");
 const _ = require('lodash');
+
+let count = 0;
+
+function updateCounter() {
+    count++;
+    $("#count").text(`${count} clicks on the button`);
+}
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
+const button = $("<button>Click here to get started</button>").click(_.debounce(updateCounter, 500, {
+    leading: true,
+    trailing: false
+}));
+$('body').append(button);
 $('body').append('<p id=\'count\'></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
-
-
-let count = 0;
-function updateCounter() {
-    count++;
-    $( "#count" ).html(`${count} clicks on the button`);
-}
-
-$('button').click(_.debounce(updateCounter, 500, { leading: true, trailing: false }));
