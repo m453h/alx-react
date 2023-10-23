3be003e49d4e797df3bb5b54fec9dfbd7d479dbe
Michael Hudson <mnkotagu@gmail.com>
2023-10-23 18:12:12 +0300

    Attempt to fix failed check 7

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -11,11 +11,11 @@ function updateCounter() {
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 
-const button = $("<button>Click here to get started</button>").on(
+const $button = $("<button>Click here to get started</button>").on(
     'click',
-    _.debounce(updateCounter, 1000, {'leading': true, 'trailing': false})
+    _.debounce(updateCounter, 500, {'leading': true, 'trailing': false})
 );
 
-$('body').append(button);
+$('body').append($button);
 $('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
