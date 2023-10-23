0882312fd2d5d18f678e9d3f64d2690a5be4e341
Michael Hudson <mnkotagu@gmail.com>
2023-10-23 20:03:25 +0300

    Last attempt to improve Task 1 solution

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,18 +1,18 @@
-const $ = require("jquery");
-const _ = require('lodash');
+import $ from 'jquery';
+import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append("<p id='count' data-click-count='0'></p>");
+$('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
 
+let counter = 0;
+
 function updateCounter() {
-    let count = parseInt($('#count').data('click-count'));
-    count++;
-    $("#count").html(`${count} clicks on the button`);
-    $('#count').data('click-count', count);
+    counter++;
+    $("#count").html(`${counter} clicks on the button`);
 }
 
 $('button').on('click', _.debounce(updateCounter, 500));
