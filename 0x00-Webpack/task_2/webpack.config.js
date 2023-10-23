bff27e46933fbc31644eaf1a264ef20f9a18c6e5
Michael Hudson <mnkotagu@gmail.com>
2023-10-23 21:23:41 +0300

    Implement solution for Task 2 - Adding CSS & Images

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        main: path.resolve(__dirname, './js/dashboard_main.js'),
+    },
+    performance: {
+        maxAssetSize: 1000000,
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+        ],
+    },
+};
