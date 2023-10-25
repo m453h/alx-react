8832886147a8810c75e3971b68c0efe723944ade
Michael Hudson <mnkotagu@gmail.com>
2023-10-26 01:33:26 +0300

    Implement solution for Task 11

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './App.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {Notifications} from "./Notifications";
+import './App/App.css';
+import App from './App/App';
+import {Notifications} from "./Notifications/Notifications";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
