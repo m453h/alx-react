04915ba1aeff59b591b897507704513856b432dd
Michael Hudson <mnkotagu@gmail.com>
2023-10-25 20:03:06 +0300

    Implement solution for Task 0

---
+++ 0x00-react_introduction/task_0/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import logo from './Holberton-logo.jpg';
+
+
+function App() {
+  return (
+      <div className='App'>
+        <div className='App-header'>
+            <img src={ logo } alt='Holberton Logo' />
+          <h1>School dashboard</h1>
+        </div>
+        <div className='App-body'>
+          <p>Login to access the full dashboard</p>
+        </div>
+        <div className='App-footer'>
+          <p>Copyright 2020 - holberton School</p>
+        </div>
+      </div>
+  );
+}
+
+export default App;
