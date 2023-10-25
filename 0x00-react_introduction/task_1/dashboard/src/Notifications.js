9ae42f4410f2243185088aad9bc7fe77066a2bd8
Michael Hudson <mnkotagu@gmail.com>
2023-10-25 22:00:03 +0300

    Improve task 3 solution

--- 0x00-react_introduction/task_1/dashboard/src/Notifications.js
+++ 0x00-react_introduction/task_1/dashboard/src/Notifications.js
@@ -4,7 +4,7 @@ import './Notifications.css';
 export function Notifications() {
     return (
         <div className='Notifications'>
-
+            <p>Here is the list of notifications</p>
         </div>
     );
 }
