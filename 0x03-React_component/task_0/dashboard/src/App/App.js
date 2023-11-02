0cdbe110ed37c39a620a85a0822205e92c2bccac
Michael Hudson <mnkotagu@gmail.com>
2023-11-02 13:38:04 +0300

    Improve App.js file

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -8,29 +8,32 @@ import PropTypes from 'prop-types';
 import CourseList from "../CourseList/CourseList";
 import { getLatestNotification } from "../utils/utils";
 
-const listCourses = [
-    { id: 1, name: "ES6", credit: 60 },
-    { id: 2, name: "Webpack", credit: 20 },
-    { id: 3, name: "React", credit: 40 },
-];
 
-const listNotifications = [
-    { id: 1, type: "default", value: "New course available" },
-    { id: 2, type: "urgent", value: "New resume available" },
-    { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
-];
 
 class App extends Component {
+
+    listCourses = [
+        { id: 1, name: "ES6", credit: 60 },
+        { id: 2, name: "Webpack", credit: 20 },
+        { id: 3, name: "React", credit: 40 },
+    ];
+
+    listNotifications = [
+        { id: 1, type: "default", value: "New course available" },
+        { id: 2, type: "urgent", value: "New resume available" },
+        { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
+    ];
+
     render() {
         return (
             <React.Fragment>
                 <div className='root-notifications'>
-                    <Notifications listNotifications={listNotifications} />
+                    <Notifications listNotifications={this.listNotifications} />
                 </div>
                 <div className="App">
                     <Header />
                     <div className="App-body">
-                        {this.props.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+                        {this.props.isLoggedIn ? <CourseList listCourses={this.listCourses} /> : <Login />}
                     </div>
                     <div className='App-footer'>
                         <Footer />
