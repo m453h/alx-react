91bd51e11263fbb5c31956f156d5cf89a7094af6
Michael Hudson <mnkotagu@gmail.com>
2023-11-02 14:16:35 +0300

    Use consitent quotes in App.js

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Notifications from "../Notifications/Notifications";
-import Header from "../Header/Header";
-import Login from "../Login/Login";
-import Footer from "../Footer/Footer";
-import "./App.css";
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import './App.css';
 import PropTypes from 'prop-types';
-import CourseList from "../CourseList/CourseList";
-import { getLatestNotification } from "../utils/utils";
+import CourseList from '../CourseList/CourseList';
+import { getLatestNotification } from '../utils/utils';
 
 
 
@@ -27,13 +27,15 @@ class App extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <Notifications listNotifications={this.listNotifications} />
+                <div className="root-notifications">
+                    <Notifications listNotifications={this.listNotifications} />
+                </div>
                 <div className="App">
                     <Header />
                     <div className="App-body">
                         {this.props.isLoggedIn ? <CourseList listCourses={this.listCourses} /> : <Login />}
                     </div>
-                    <div className='App-footer'>
+                    <div className="App-footer">
                         <Footer />
                     </div>
                 </div>
