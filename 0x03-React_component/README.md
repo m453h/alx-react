b8a042dfda6b94617174c99445f32b5d55b1e468
Michael Hudson <mnkotagu@gmail.com>
2023-11-02 12:51:08 +0300

    Implement solution for Task 0

---
+++ 0x03-React_component/README.md
@@ -0,0 +1,18 @@
+# 0x02. React props
+
+## Learning Objectives
+This project introduces the concept of React Props, it mainly covers the following concepts:
+
+- How to create basic React components using functions
+- How to reuse components
+- How to pass properties to components
+- How to define types for components
+- How to use Fragments
+- When to use a key to improve a loop’s performance
+
+## Requirements
+
+- All files will be interpreted/compiled on Ubuntu 18.04 LTS using node `12.x.x` and npm `6.x.x`
+- Allowed editors: `vi`, `vim`, `emacs`, `Visual Studio Code`
+- All files should end with a new line
+- A `README.md` file, at the root of the folder of the project, is mandatory
---
+++ 0x03-React_component/task_0/dashboard/src/App/App.test.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
+import Header from '../Header/Header';
+import Notifications from '../Notifications/Notifications';
+import CourseList from "../CourseList/CourseList";
+import {getLatestNotification} from "../utils/utils";
+
+describe('App Component rendering tests', () => {
+    it('renders <App /> without crashing', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper).toBeDefined();
+    });
+
+    it("contains Notifications component", () => {
+        const listNotifications = [
+            { id: 1, type: "default", value: "New course available" },
+            { id: 2, type: "urgent", value: "New resume available" },
+            { id: 3, type: "urgent", html: {__html: getLatestNotification()}},
+        ];
+        const component = shallow(<App />);
+        expect(component.contains(<Notifications listNotifications={listNotifications}/>)).toBe(true);
+    });
+
+    it("contains Header component", () => {
+        const component = shallow(<App />);
+        expect(component.contains(<Header />)).toBe(true);
+    });
+
+    it("contains Login Component", () => {
+        const component = shallow(<App />);
+        expect(component.contains(<Login />)).toBe(true);
+    });
+
+    it("contains Footer component", () => {
+        const component = shallow(<App />);
+        expect(component.contains(<Footer />)).toBe(true);
+    });
+
+});
+
+describe('When App isLoggedin is True', function () {
+    it('does not render Login component', function () {
+        const wrapper = shallow(<App isLoggedIn={true} />);
+        expect(wrapper.find(Login).length).toBe(0);
+    });
+
+    it('renders the CourseList component', function () {
+        const wrapper = shallow(<App isLoggedIn={true} />);
+        expect(wrapper.find(CourseList).length).toBe(1);
+    });
+});
