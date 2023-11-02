067e5e206713105ff2a6e4b1865c3aca01b2c55e
Michael Hudson <mnkotagu@gmail.com>
2023-11-02 13:32:42 +0300

    Improve App.js to fix failed check-1

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component } from 'react';
 import Notifications from "../Notifications/Notifications";
 import Header from "../Header/Header";
 import Login from "../Login/Login";
@@ -20,7 +20,7 @@ const listNotifications = [
     { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
 ];
 
-class App extends React.Component {
+class App extends Component {
     render() {
         return (
             <React.Fragment>
