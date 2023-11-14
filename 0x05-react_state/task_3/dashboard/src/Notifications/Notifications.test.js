0d2fed06f6d190c789a2e6d09ad3d337767ad006
Michael Hudson <mnkotagu@gmail.com>
2023-11-14 03:27:10 +0300

    Clean up tests for Task 3

--- 0x05-react_state/task_3/dashboard/src/Notifications/Notifications.test.js
+++ 0x05-react_state/task_3/dashboard/src/Notifications/Notifications.test.js
@@ -73,47 +73,41 @@ describe('Notifications Component rendering tests', () => {
         expect(wrapper.find('p').text()).not.toContain('Here is the list of notifications');
     });
 
-    it('checks that when calling the function markAsRead, the spy is being called with the right message', () => {
-        const consoleLogSpy = jest.spyOn(console, 'log');
-
-        const listNotifications = [
-            { id: 1, type: "default", value: "New course available" },
-            { id: 2, type: "urgent", value: "New resume available" },
-            { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
-        ];
-
-        const wrapper = shallow(<Notifications displayDrawer listNotifications={listNotifications} />);
-        const instance = wrapper.instance();
-        instance.markAsRead(listNotifications[2].id);
-        expect(consoleLogSpy).toHaveBeenCalledWith(`Notification ${listNotifications[2].id} has been marked as read`);
-        consoleLogSpy.mockRestore();
-    });
-
-    it("checks that the component does not render when updated with same list", () => {
-        const initialNotifications = [
-            { id: 1, type: "default", value: "New course available" },
-            { id: 2, type: "urgent", value: "New resume available" },
-            { id: 3, type: "urgent", value: "Urgent update is available" },
-        ];
-        const wrapper = shallow(<Notifications displayDrawer={true} listNotifications={initialNotifications} />);
-        expect(wrapper.instance().shouldComponentUpdate(initialNotifications)).toBe(true);
-    });
-
-    it("checks that the component renders when updated with a longer list", () => {
+   it("checks that the component does not render when updated with same list", () => {
         const initialNotifications = [
             { id: 1, type: "default", value: "New course available" },
             { id: 2, type: "urgent", value: "New resume available" },
             { id: 3, type: "urgent", value: "Urgent update is available" },
         ];
 
-        const updatedNotifications = [
+       const wrapper = shallow(<Notifications displayDrawer listNotifications={initialNotifications}/>);
+       const initialNumberOfNotifications = wrapper.find(NotificationItem).length;
+       wrapper.setProps({ listNotifications: initialNotifications });
+
+       const currentNumberOfNotificationItems = wrapper.find(NotificationItem).length;
+       expect(currentNumberOfNotificationItems).toBe(initialNumberOfNotifications);
+    });
+
+   it("checks that the component renders when updated with a longer list", () => {
+        const initialNotifications = [
+            { id: 1, type: "default", value: "New course available" },
+            { id: 2, type: "urgent", value: "New resume available" },
+            { id: 3, type: "urgent", value: "Urgent update is available" },
+        ];
+       const wrapper = shallow(<Notifications displayDrawer listNotifications={initialNotifications}/>);
+       const initialNumberOfNotifications = wrapper.find(NotificationItem).length;
+
+       const updatedNotifications = [
             ...initialNotifications,
             { id: 4, type: 'default', value: 'New update is available' },
         ];
 
-        const wrapper = shallow(<Notifications displayDrawer={true} listNotifications={initialNotifications} />);
-        expect(wrapper.instance().shouldComponentUpdate(updatedNotifications)).toBe(true);
-    });
+       wrapper.setProps({ listNotifications: updatedNotifications });
+       const currentNumberOfNotificationItems = wrapper.find(NotificationItem).length;
+
+       expect(currentNumberOfNotificationItems).toBeGreaterThan(initialNumberOfNotifications);
+
+   });
 
     it("calls handleDisplayDrawer after clicking on the menu item", () => {
         const handleDisplayDrawer = jest.fn();
