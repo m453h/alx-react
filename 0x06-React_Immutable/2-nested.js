a596b9593991c3680108c264950a066b737f7029
Michael Hudson <mnkotagu@gmail.com>
2023-11-16 02:32:54 +0300

    Improve 2-nested.js to use fromJS instead of Map

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,6 +1,6 @@
-import { Map } from 'immutable';
+import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  const objectMap = Map(object);
+  const objectMap = fromJS(object);
   return objectMap.getIn(array, undefined);
 }
