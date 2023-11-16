db75d0785b5c5aad448a98a24b627d5396b3f256
Michael Hudson <mnkotagu@gmail.com>
2023-11-16 16:18:16 +0300

    Improve 8-seq.js solution

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -2,11 +2,11 @@ import { Seq, fromJS } from 'immutable';
 
 export default function printBestStudents(object) {
   const studentsSeq = Seq(fromJS(object));
-  const bestStudents = studentsSeq.filter((student) => student.get('score') >= 70);
-  const formattedBestStudents = bestStudents.map((student) => student
-    .update('firstName', (firstName) => firstName.charAt(0).toUpperCase() + firstName.slice(1))
-    .update('lastName', (lastName) => lastName.charAt(0).toUpperCase() + lastName.slice(1))
-  )
+  const bestStudentsFormatted = studentsSeq
+    .filter((student) => student.get('score') >= 70)
+    .map((student) => student
+      .update('firstName', (firstName) => firstName.charAt(0).toUpperCase() + firstName.slice(1))
+      .update('lastName', (lastName) => lastName.charAt(0).toUpperCase() + lastName.slice(1)))
     .toJS();
-  console.log(formattedBestStudents);
+  console.log(bestStudentsFormatted);
 }
