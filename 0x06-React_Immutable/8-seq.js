6d87b57a96bac9ede3f5489e4be2efdadf44d151
Michael Hudson <mnkotagu@gmail.com>
2023-11-16 15:08:40 +0300

    Improve 8-seq.js to comply with eslint requirements

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -2,20 +2,12 @@ import { Seq, fromJS } from 'immutable';
 
 export default function printBestStudents(object) {
   const studentsSeq = Seq(fromJS(object));
-
-  const bestStudents = studentsSeq
-    .filter((student) => {
-      return student.get('score') >= 70;
-    })
-    .map((student) => {
-      return student
-        .update('firstName', (firstName) => {
-          return firstName.charAt(0).toUpperCase() + firstName.slice(1);
-        })
-        .update('lastName', (lastName) => {
-          return lastName.charAt(0).toUpperCase() + lastName.slice(1);
-        });
-    }).toJS();
-
+  const bestStudents = studentsSeq.filter((student) => student.get('score') >= 70);
+  bestStudents.map((student) =>
+      student
+        .update('firstName', (firstName) => firstName.charAt(0).toUpperCase() + firstName.slice(1))
+        .update('lastName', (lastName) => lastName.charAt(0).toUpperCase() + lastName.slice(1))
+    )
+    .toJS();
   console.log(bestStudents);
 }
