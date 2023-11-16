df4c5e82ed4042d79c2695b2bf912605c3bb3680
Michael Hudson <mnkotagu@gmail.com>
2023-11-16 13:33:10 +0300

    Improve 6-deeply.js

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,7 +1,8 @@
 import { Map } from 'immutable';
 
-export default function mergeDeeplyElements (page1, page2) {
+export default function mergeDeeplyElements(page1, page2) {
   const page1Map = Map(page1);
   const page2Map = Map(page2);
+
   return page1Map.mergeDeep(page2Map);
 }
