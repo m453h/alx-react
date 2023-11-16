fddbf812601809e291858e961b28e25e62dec886
Michael Hudson <mnkotagu@gmail.com>
2023-11-16 16:05:16 +0300

    Improve linting in 8-seq.js

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -4,9 +4,9 @@ export default function printBestStudents(object) {
   const studentsSeq = Seq(fromJS(object));
   const bestStudents = studentsSeq.filter((student) => student.get('score') >= 70);
   const formattedBestStudents = bestStudents.map((student) => student
-  .update('firstName', (firstName) => firstName.charAt(0).toUpperCase() + firstName.slice(1))
-  .update('lastName', (lastName) => lastName.charAt(0).toUpperCase() + lastName.slice(1))
-)
-.toJS();
-console.log(formattedBestStudents);
+    .update('firstName', (firstName) => firstName.charAt(0).toUpperCase() + firstName.slice(1))
+    .update('lastName', (lastName) => lastName.charAt(0).toUpperCase() + lastName.slice(1))
+  )
+    .toJS();
+  console.log(formattedBestStudents);
 }
