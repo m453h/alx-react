fede4f8e78686cfb1269322ec1ebaa4370830286
Michael Hudson <mnkotagu@gmail.com>
2023-11-16 16:35:05 +0300

    Refractor 8-seq.js solution

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,12 +1,14 @@
 import { Seq, fromJS } from 'immutable';
 
+const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
+
 export default function printBestStudents(object) {
   const studentsSeq = Seq(fromJS(object));
   const bestStudentsFormatted = studentsSeq
     .filter((student) => student.get('score') >= 70)
     .map((student) => student
-      .update('firstName', (firstName) => firstName.charAt(0).toUpperCase() + firstName.slice(1))
-      .update('lastName', (lastName) => lastName.charAt(0).toUpperCase() + lastName.slice(1)))
+      .update('firstName', capitalize)
+      .update('lastName', capitalize))
     .toJS();
   console.log(bestStudentsFormatted);
 }
