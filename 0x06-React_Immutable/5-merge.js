c933256a23098cd1009b48d91cb887188e53729c
Michael Hudson <mnkotagu@gmail.com>
2023-11-16 11:59:24 +0300

    Improve 5-merge.js

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -6,7 +6,7 @@ export function concatElements(page1, page2) {
   return page1List.concat(page2List);
 }
 
-export function addElementToList(page1, page2) {
+export function mergeElements(page1, page2) {
   const page1Map = Map(page1);
   const page2Map = Map(page2);
   return page1Map.merge(page2Map);
