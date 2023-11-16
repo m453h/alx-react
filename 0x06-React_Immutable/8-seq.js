65e0b9d2b510918cfac2a37e49f9a4cb50c9aa91
Michael Hudson <mnkotagu@gmail.com>
2023-11-16 16:43:52 +0300

    Fix linting issues in refractored 8-seq.js

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,6 +1,6 @@
 import { Seq, fromJS } from 'immutable';
 
-const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
 export default function printBestStudents(object) {
   const studentsSeq = Seq(fromJS(object));
