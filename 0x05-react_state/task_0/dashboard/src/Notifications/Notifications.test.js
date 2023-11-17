769ccd08d18015b20a14037df07c2813ea27e0c3
Michael Hudson <mnkotagu@gmail.com>
2023-11-18 01:44:58 +0300

    Improve Notifications.test.js

--- 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.test.js
+++ 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.test.js
@@ -73,22 +73,6 @@ describe('Notifications Component rendering tests', () => {
         expect(wrapper.find('p').text()).not.toContain('Here is the list of notifications');
     });
 
-    it('checks that when calling the function markAsRead, the spy is being called with the right message', () => {
-        const consoleLogSpy = jest.spyOn(console, 'log');
-
-        const listNotifications = [
-            { id: 1, type: "default", value: "New course available" },
-            { id: 2, type: "urgent", value: "New resume available" },
-            { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
-        ];
-
-        const wrapper = shallow(<Notifications displayDrawer listNotifications={listNotifications} />);
-        const instance = wrapper.instance();
-        instance.markAsRead(listNotifications[2].id);
-        expect(consoleLogSpy).toHaveBeenCalledWith(`Notification ${listNotifications[2].id} has been marked as read`);
-        consoleLogSpy.mockRestore();
-    });
-
     it("checks that the component does not render when updated with same list", () => {
         const initialNotifications = [
             { id: 1, type: "default", value: "New course available" },
