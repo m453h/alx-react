152b0670a97153748a1f1000bb080aa79c2528b9
Michael Hudson <mnkotagu@gmail.com>
2023-11-23 23:10:31 +0300

    Move schema directory to src

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -1,4 +1,4 @@
-import notifications from '../notifications.json';
+import notifications from '../../notifications.json';
 
 export default function getAllNotificationsByUser(userId) {
     const filteredNotifications = notifications.filter((notification) => {
