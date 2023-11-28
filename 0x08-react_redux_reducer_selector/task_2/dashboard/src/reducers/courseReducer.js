9c6bae4522b4ec6c205cdd65b21417d02aceeb33
Michael Hudson <mnkotagu@gmail.com>
2023-11-28 21:08:13 +0300

    Make courseReducer more readable

--- 0x08-react_redux_reducer_selector/task_2/dashboard/src/reducers/courseReducer.js
+++ 0x08-react_redux_reducer_selector/task_2/dashboard/src/reducers/courseReducer.js
@@ -9,23 +9,28 @@ const initialState = [];
 export default function courseReducer(state = initialState, action) {
     switch (action.type) {
         case FETCH_COURSE_SUCCESS:
-            return action.data.map(function(course) {
+            return action.data.map(function (course) {
                 return { ...course, isSelected: false };
             });
 
-        case SELECT_COURSE:
+        case SELECT_COURSE: {
+            const { index } = action;
+            return state.map(function (course) {
+                return {
+                    ...course,
+                    isSelected: course.id === index ? true : course.isSelected,
+                };
+            });
+        }
+
         case UNSELECT_COURSE: {
             const { index } = action;
-            if (index >= 0 && index < state.length) {
-                const isSelected = action.type === SELECT_COURSE;
-                return state.map(function(course) {
-                    return {
-                        ...course,
-                        isSelected: course.id === index ? isSelected : course.isSelected
-                    };
-                });
-            }
-            return state;
+            return state.map(function (course) {
+                return {
+                    ...course,
+                    isSelected: course.id === index ? false : course.isSelected,
+                };
+            });
         }
 
         default:
