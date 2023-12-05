02ca01bdef501946883be62e1cab1fd15828572f
Michael Hudson <mnkotagu@gmail.com>
2023-12-05 18:28:55 +0300

    Improve Task 13 solution

--- 0x09-React_Redux_reducer/task_3/dashboard/src/index.js
+++ 0x09-React_Redux_reducer/task_3/dashboard/src/index.js
@@ -13,7 +13,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     uiReducer,
     Map(initialState),
-    (applyMiddleware(thunk))
+    composeEnhancers(applyMiddleware(thunk))
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
