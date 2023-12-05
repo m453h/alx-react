9dbf985a66718561b0b8339b7b06b1509facef48
Michael Hudson <mnkotagu@gmail.com>
2023-12-05 22:18:19 +0300

    Implement solution for Task 18

--- 0x09-React_Redux_reducer/task_5/dashboard/src/reducers/notificationReducer.js
+++ 0x09-React_Redux_reducer/task_5/dashboard/src/reducers/notificationReducer.js
@@ -1,26 +1,32 @@
 import {
     FETCH_NOTIFICATIONS_SUCCESS,
-    MARK_AS_READ, NotificationTypeFilters,
+    MARK_AS_READ, NotificationTypeFilters, SET_LOADING_STATE,
     SET_TYPE_FILTER
 } from '../actions/notificationActionTypes';
 import {Map, setIn} from 'immutable';
 import {notificationsNormalizer} from "../schema/notifications";
 
 
-export const initialState = [];
-export default function notificationReducer(state = initialState, action) {
+export const initialState = {
+    notifications: [],
+    filter: 'DEFAULT',
+    loading: false,
+};
+
+export default function notificationReducer(state = Map(initialState), action) {
     switch (action.type) {
         case FETCH_NOTIFICATIONS_SUCCESS:
             const data = action.data.map(notification => ({ ...notification, isRead: false }));
-            return state.merge({
+            return state.mergeDeep({
                 filter: NotificationTypeFilters.DEFAULT,
                 notifications: notificationsNormalizer(data),
             });
-
         case MARK_AS_READ:
             return setIn(state, ['notifications', action.index.toString(), 'isRead'], true);
         case SET_TYPE_FILTER:
             return Map(state).set('filter', action.filter);
+        case SET_LOADING_STATE:
+            return state.set('loading', action.loading);
         default:
             return state;
     }
