33d92b96897fe0213ba993e4f92860be857311ce
Michael Hudson <mnkotagu@gmail.com>
2023-12-05 18:31:05 +0300

    Attempt to implement further improvements

--- 0x09-React_Redux_reducer/task_3/dashboard/src/index.js
+++ 0x09-React_Redux_reducer/task_3/dashboard/src/index.js
@@ -7,6 +7,7 @@ import {applyMiddleware, compose, createStore} from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { Map } from 'immutable';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
