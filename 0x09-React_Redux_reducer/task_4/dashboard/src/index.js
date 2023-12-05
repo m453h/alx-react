e620d624914f7e1810762b049f8704bb851a3e60
Michael Hudson <mnkotagu@gmail.com>
2023-12-05 18:58:58 +0300

    Implement Task 14 solution

--- 0x09-React_Redux_reducer/task_4/dashboard/src/index.js
+++ 0x09-React_Redux_reducer/task_4/dashboard/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App/App';
 import reportWebVitals from "./reportWebVitals";
 import uiReducer from "./reducers/uiReducer";
-import {applyMiddleware, createStore, compose} from "redux";
+import {applyMiddleware, compose, createStore} from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from 'redux-devtools-extension';
