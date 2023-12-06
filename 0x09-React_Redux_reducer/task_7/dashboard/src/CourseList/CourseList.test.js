e574f544315d49b26faf5f3d8137f5e11abecd37
Michael Hudson <mnkotagu@gmail.com>
2023-12-06 20:52:36 +0300

    Implement CourseList.test.js for Task 25

--- 0x09-React_Redux_reducer/task_7/dashboard/src/CourseList/CourseList.test.js
+++ 0x09-React_Redux_reducer/task_7/dashboard/src/CourseList/CourseList.test.js
@@ -1,8 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
 import React from 'react';
-import { shallow } from 'enzyme';
-import CourseList from './CourseList';
+import {mount, shallow} from 'enzyme';
+import { CourseList } from './CourseList';
 import CourseListRow from './CourseListRow';
 import {StyleSheetTestUtils} from "aphrodite";
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+
+const mockStore = configureStore();
+const store = mockStore({}); // Pass an initial state that matches your Redux store structure
+
 
 describe('CourseList component tests', () => {
     beforeEach(() => {
@@ -14,34 +23,44 @@ describe('CourseList component tests', () => {
     });
 
     it('renders without crashing', () => {
-        const wrapper = shallow(<CourseList />);
-        expect(wrapper).toBeDefined();
+        const wrapper = shallow(<CourseList listCourses={[]} />);
+        expect(wrapper.exists()).toEqual(true);
     });
 
     describe('tests when listCourses is Empty', () => {
-        let wrapper;
-        beforeEach(() => {
-            wrapper = shallow(<CourseList />);
-        });
         it('renders correctly if you pass an empty array', () => {
+            const wrapper = shallow(<CourseList />);
             const courseListRows = wrapper.find(CourseListRow);
-            expect(courseListRows).toHaveLength(3);
+            expect(courseListRows).toEqual({});
         });
     });
 
     describe('tests when listCourses is not empty', () => {
-        let wrapper;
-        beforeEach(() => {
+        it('renders correctly if you pass a non empty array', () => {
             const listCourses = [
                 { id: 1, name: "ES6", credit: 60 },
                 { id: 2, name: "Webpack", credit: 20 },
                 { id: 3, name: "React", credit: 40 },
             ];
-            wrapper = shallow(<CourseList listCourses={listCourses}/>);
-        });
-        it('renders correctly if you pass a non empty array', () => {
+            const wrapper = shallow(<CourseList listCourses={ listCourses} />);
             const courseListRows = wrapper.find(CourseListRow);
             expect(courseListRows).toHaveLength(5);
         });
+    });
+
+    it('dispatches selectCourse when checked is true', () => {
+        const mockSelectCourse = jest.fn();
+        const rowId = 100;
+        const wrapper = mount(<CourseList listCourses={[]} selectCourse={mockSelectCourse}/>);
+        wrapper.instance().onChangeRow(rowId, true);
+        expect(mockSelectCourse).toHaveBeenCalledWith(rowId);
+    });
+
+    it('dispatches unSelectCourse when checked is false', () => {
+        const mockUnSelectCourseMock = jest.fn();
+        const rowId = 200;
+        const wrapper = mount(<CourseList listCourses={[]} unSelectCourse={mockUnSelectCourseMock}/>);
+        wrapper.instance().onChangeRow(rowId, false);
+        expect(mockUnSelectCourseMock).toHaveBeenCalledWith(rowId);
     });
 });
